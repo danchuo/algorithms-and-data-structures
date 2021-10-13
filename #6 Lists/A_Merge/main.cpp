27a923182cbc2b302b353305b2cd91d27206a130
dontyouo <danchuodanchuo@gmail.com>
2021-10-13 10:42:07 +0300

    .gitignore changed

---
+++ #6 Lists/A_Merge/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << "\n";
+    return 0;
+}

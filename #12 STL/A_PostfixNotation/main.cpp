7f672789b4f7c2a6dd5deecfc3ef3d0f0dd0be3c
dontyouo <danchuodanchuo@gmail.com>
2021-12-16 15:32:42 +0300

    WA3

---
+++ #12 STL/A_PostfixNotation/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+std::string createPostfixNotation(std::string input) {
+    std::string answer;
+    answer.reserve(input.size());
+
+    auto *stack = new std::stack<char>();
+
+    char previous_symbol = '0';
+
+    for (const auto &symbol : input) {
+        if (symbol != ' ') {
+            if (std::isdigit(symbol)) {
+                if (!std::isdigit(previous_symbol)) {
+                    answer += ' ';
+                }
+                answer += symbol;
+            } else {
+                if (!stack->empty() && stack->top() == '(') {
+                    stack->push(symbol);
+                } else {
+                    if (symbol == ')') {
+                        while (stack->top() != '(') {
+                            answer += ' ';
+                            answer += stack->top();
+                            stack->pop();
+                        }
+                        stack->pop();
+                    } else {
+                        while (!stack->empty() &&
+                               (((symbol == '+' || symbol == '-') &&
+                                 (stack->top() == '+' || stack->top() == '-')) ||
+                                ((symbol == '+' || symbol == '-') &&
+                                 (stack->top() == '/' || stack->top() == '*')))) {
+                            answer += ' ';
+                            answer += stack->top();
+                            stack->pop();
+                        }
+                        stack->push(symbol);
+                    }
+                }
+            }
+        } else {
+            previous_symbol = false;
+        }
+        previous_symbol = symbol;
+    }
+
+    while (!stack->empty()) {
+        answer += ' ';
+        answer += stack->top();
+        stack->pop();
+    }
+    delete stack;
+
+    return answer;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::string sequence;
+    std::getline(std::cin, sequence);
+
+    std::cout << createPostfixNotation(sequence);
+
+    return 0;
+}
